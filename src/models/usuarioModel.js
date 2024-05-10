30a42e0bbfd1a925671f5c240c9f247c7f52d2f6
anamoraeszx <ana.moraes@sptech.school>
2024-05-10 12:24:36 -0300

    Atualização da API

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -16,7 +16,7 @@ function cadastrar(nome, cpf,  email, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, cpf, email, senha) VALUES ('${nome}', '${cpf}' '${email}', '${senha}');
+        INSERT INTO Usuario (nome, cpf, email, senha) VALUES ('${nome}', '${cpf}', '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
